c84cb54a1697598a88c2c26d7da0d0f9cb130c1c
Yulli-Lizano <yulli3lc@gmail.com>
2019-01-10 09:37:25 -0500

    se agrego link de zeplin al archivo README.md y correcciones en main.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,7 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window et
-
 const example = () => {
   return 'example';
 };
 window.example = example;
-
 const population = (arr) => {
   let populationArr = [];
   populationArr = arr.filter(populationElement => populationElement.indicatorCode.substring(0, 6) === 'SP.POP');
@@ -31,17 +27,15 @@ const unemployment = (arr) => {
   unemploymentArr = arr.filter(unemploymentElement => unemploymentElement.indicatorCode.substring(0, 6) === 'SL.UEM');
   return unemploymentArr;
 };
-
 const converToInt = (data) => {
   const result = data.map((x) => { 
     const array = []; 
     array[0] = parseInt(x[0], 10) ;
-    array[1] = x[1] ;
+    array[1] = (x[1] === '' ? 0 : parseFloat(x[1]));  
     return array; 
   });
   return result;
 };
-
 const calculateAverage = (data) => {
   const initial = 0;
   const arrSum = data.reduce((sum, current) => sum + current[1], initial); 
@@ -59,12 +53,11 @@ const filterToData = (data, string) => {
   arr = data.filter(element => element.indicatorName === string);
   return finalData(arr);
 };
-
 const getAndShowData = (datas, country, string) => {
   const worldBankIndicatorsForCountry = datas[country]['indicators'];
   return filterToData(worldBankIndicatorsForCountry, string) ;
 };
-const compare = (element1, element2) => {
+const compareDesc = (element1, element2) => {
   if (parseInt(element1[0]) < parseInt(element2[0])) {
     return 1;  
   }
@@ -73,9 +66,25 @@ const compare = (element1, element2) => {
   }
   return 0;
 };
-const sortData = (parametro) => {
-  parametro.sort(compare);
-  return parametro;
+const compareAsc = (element1, element2) => {
+  if (parseInt(element1[0]) < parseInt(element2[0])) {
+    return -1;  
+  }
+  if (parseInt(element1[0]) > parseInt(element2[0])) {
+    return 1;  
+  }
+  return 0;
+};
+const sortData = (parametro, typeOfOrder) => {
+  switch (typeOfOrder) {
+  case 'desc':
+    parametro.sort(compareDesc);
+    return parametro;
+  case 'asc':
+    parametro.sort(compareAsc);
+    parametro.sort(compareAsc);
+    return parametro;
+  }
 };
 
 
